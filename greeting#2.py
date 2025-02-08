493f6f3f7a30a7d235cf8803ee59eee8fe5e3f7c
Angelitoo2000 <angel.trejo2026@hilasal.superate.org.sv>
2025-02-07 20:46:55 -0600

    Codigo tarea de info

---
+++ greeting#2.py
@@ -0,0 +1,23 @@
+""""
+while True:
+    print("Write'Hello'to send a beautiful menssage")
+    word=input("Write your answer:")
+    if word=="Hello":
+        print("Hi sir,have a good day")
+        break
+    else:
+        print("I am sorry who are you?")
+"""
+
+
+""""
+fruits=[]
+for i in range(1,6):
+    fruit=input(f"enter a fruit {i}/5:")
+    fruits.append(fruit)
+    print(f"you enter {i} fruit")
+
+    print("fruits did you enter are")
+    for fruit in fruits:
+        print(fruit)
+        """
